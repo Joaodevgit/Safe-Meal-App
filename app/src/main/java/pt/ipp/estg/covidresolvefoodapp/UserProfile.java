bf6cf45357727cae3e4e0a53dd15ffbd9463bb0f
Joaodevgit <joaotiagopereira@sapo.pt>
2020-08-25 00:34:12 +0100

    Criação da atividade Perfil do Utilizador

--- app/src/main/java/pt/ipp/estg/covidresolvefoodapp/UserProfile.java
+++ app/src/main/java/pt/ipp/estg/covidresolvefoodapp/UserProfile.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -11,18 +13,38 @@ public class UserProfile extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private Toolbar myToolbar;
+    private TextView userNameTxtView;
+    private ImageView profileImg;
+    private TextView totalUserMeals;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
-        this.mAuth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        // Text Views
+        userNameTxtView = findViewById(R.id.userEmail);
+        //Toast.makeText(getApplicationContext(), FirebaseAuth.getInstance().getCurrentUser().getEmail(), Toast.LENGTH_SHORT).show();
+        userNameTxtView.setText("Email: " + mAuth.getCurrentUser().getEmail());
+
+        totalUserMeals = findViewById(R.id.textViewTotalMeals);
+        // Falta fazer a contagem das refeições efetuadas pelo cliente
+        totalUserMeals.setText("Nº total de refeições efetuadas: 6");
+
+        // Image Views
+        profileImg = findViewById(R.id.imageViewProfile);
+        profileImg.setImageResource(R.drawable.ic_user_profile_img);
+
 
         this.myToolbar = findViewById(R.id.toolbarUser);
         setSupportActionBar(this.myToolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
     }
 
     @Override
