aeae1778b5c42ba4a06c2d2bc2dad4fe5132918b
Paulo Pinto Da Cunha <8170257@estg.ipp.pt>
2020-09-02 15:18:37 +0100

    Correção infoRestaurant

--- app/src/main/java/pt/ipp/estg/covidresolvefoodapp/SearchRestaurant/InfoRestaurantActivity.java
+++ app/src/main/java/pt/ipp/estg/covidresolvefoodapp/SearchRestaurant/InfoRestaurantActivity.java
@@ -1,9 +1,7 @@
 package pt.ipp.estg.covidresolvefoodapp.SearchRestaurant;
 
-import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
